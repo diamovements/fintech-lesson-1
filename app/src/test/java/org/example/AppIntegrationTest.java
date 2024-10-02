89c9df97c2f8733ee7d09848b5904e1c3a9263a2
Абдукаримова_Карина <yuukiasuna0@bk.ru>
2024-10-02 14:22:12 +0300

    refactored integration test

--- app/src/test/java/org/example/AppIntegrationTest.java
+++ app/src/test/java/org/example/AppIntegrationTest.java
@@ -6,17 +6,19 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.web.client.RestTemplate;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-
 import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -31,14 +33,17 @@ public class AppIntegrationTest {
     @Autowired
     private RestTemplate restTemplate;
 
+    @DynamicPropertySource
+    static void wireMockProperties(DynamicPropertyRegistry registry) {
+        registry.add("app.url", () -> "http://localhost:" + wireMockServer.port() + "/public-api/v1.4/");
+    }
 
     @BeforeAll
     public static void setUp() {
-        postgreSQLContainer.start();
-
-        wireMockServer = new WireMockServer(8081);
+        wireMockServer = new WireMockServer(options().dynamicPort());
         wireMockServer.start();
-        configureFor("localhost", 8081);
+        configureFor("localhost", wireMockServer.port());
+        postgreSQLContainer.start();
     }
 
     @AfterAll
@@ -51,24 +56,24 @@ public class AppIntegrationTest {
 
     @Test
     void getCategories_shouldReturnCategories() {
-        stubFor(get(urlEqualTo("/public-api/v1.4/place-categories"))
+        stubFor(get(urlEqualTo("/place-categories"))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
                         .withBody("[{\"id\": 9, \"slug\": \"homesteads\", \"name\": \"Усадьбы\"}]")));
 
-        String response = restTemplate.getForObject("http://localhost:8081/public-api/v1.4/place-categories", String.class);
+        String response = restTemplate.getForObject("http://localhost:" + wireMockServer.port() + "/place-categories", String.class);
         assertThat(response).contains("Усадьбы");
     }
 
     @Test
     void getPlaces_shouldReturnPlaces() {
-        stubFor(get(urlEqualTo("/public-api/v1.4/places"))
+        stubFor(get(urlEqualTo("/locations"))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
                         .withBody("[{\"slug\": \"msk\", \"name\": \"Москва\"}]")));
-        String response = restTemplate.getForObject("http://localhost:8081/public-api/v1.4/places", String.class);
+        String response = restTemplate.getForObject("http://localhost:" + wireMockServer.port() + "/locations", String.class);
         assertThat(response).contains("Москва");
     }
 }
